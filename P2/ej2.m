232cc042434f6e87a9be19a126fadc3e38e5e7da
hanon11 <lucia.atienza2@gmail.com>
2022-03-18 11:40:37 +0100

    P2

--- P2/ej2.m
+++ P2/ej2.m
@@ -6,8 +6,11 @@ T = readmatrix('winequality-red.csv');
 x = T(:, 1:width(T)-1);
 y = T(:, width(T)); %calidad de los vinos
 A = [x x.^2 ones(height(x), 1)];
-sol = inv(A'*A)*(A'*y)
-yEstimada = y-A*sol;
+coefs = pinv(A)*y;
+yEstimada = y-A*coefs;
 r = sum(abs(yEstimada));
-E = r/height(x);
-Ecuadrado = (yEstimada'*yEstimada)/height(x);
+E_medio = r/height(x);
+E_medio_cuadrado = (yEstimada'*yEstimada)/height(x);
+
+% en comparacion con el ejercicio anterior, tanto el error medio como el e.
+% medio cuadrado disminuyen 
--- P2/ej3.m
+++ P2/ej3.m
@@ -4,23 +4,23 @@ clc;
 x = [-1 : 0.1 : 1]';
 y = [5*x.^3 - x.^2 + 2*x + 1];
 A = [x ones(size(x))];
-sol = inv(A'*A)*(A'*y);
-figure,hold on; plot (x,y,'o'); plot(x,A*sol,'r'),title('Ajuste 1');
-plot(x,A*mipolyfit(x,y,1),'*b');
+coefs = pinv(A)*y;
+figure,hold on; plot (x,y,'o'); plot(x,A*coefs,'r'),title('Ajuste 1');
+plot(x,A*mipolyfit(x,y,1),'*b'); % pinto mi polyfit con asteriscos para verlo mejor, las graficas se superponen (como debe ser)
 legend("Puntos", "Primer ajuste", "mipolyfit");
 hold off; 
 
 A = [x x.^2 ones(size(x))];
-sol = inv(A'*A)*(A'*y);
-figure,hold on; plot (x,y,'o'); plot (x,A*sol,'r'),title('Ajuste 2');
+coefs = pinv(A)*y;
+figure,hold on; plot (x,y,'o'); plot (x,A*coefs,'r'),title('Ajuste 2');
 plot(x,A*mipolyfit(x,y,2),'*b');
 legend("Puntos", "Primer ajuste", "mipolyfit");
 hold off; 
 
 
 A = [x x.^2  x.^3 ones(size(x))];
-sol = inv(A'*A)*(A'*y);
-figure,hold on; plot (x,y,'o'); plot (x,A*sol,'r'),title('Ajuste 3');
+coefs = pinv(A)*y;
+figure,hold on; plot (x,y,'o'); plot (x,A*coefs,'r'),title('Ajuste 3');
 plot(x,A*mipolyfit(x,y,3),'*b');
 legend("Puntos", "Primer ajuste", "mipolyfit");
 hold off; 
