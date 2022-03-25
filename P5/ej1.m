95f674a1969ab8a24f0c64d6430a11336fad6987
hanon11 <lucia.atienza2@gmail.com>
2022-03-25 12:09:14 +0100

    P5

--- P5/ej1.m
+++ P5/ej1.m
@@ -6,7 +6,7 @@ rand('seed', 0);
 randn('seed', 0);
 
 x1 = randnorm([0,0], [1 0.8; 0.8 2], 1000);
-x2 = randnorm([3,3], [1 -0.9;0.9 2], 1000);
+x2 = randnorm([3,3], [1 -0.9;-0.9 2], 1000);
 x = [x1 x2];
 y = [zeros(1,1000) ones(1,1000)]; 
 [x,y] = shuffle(x,y);
