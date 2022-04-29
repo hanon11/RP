307d328c7ca29328f47498ad367a44b57da55993
hanon11 <lucia.atienza2@gmail.com>
2022-04-29 11:49:12 +0200

    P8

--- P8/P8_ej2.m
+++ P8/P8_ej2.m
@@ -4,11 +4,10 @@ clc;
 rng(0);
 
 x_= randnorm([3 4], [1 0.8 ; 0.8 1], 1000);
-%x_ = x_ - [3 ; 4]; % restamos la media
 x__= randnorm([5 0], [1 0.8 ; 0.8 1], 1000);
-%x__ = x__ - [5 ; 0]; % restamos la media
 
 x=[x_ x__];
+x = x-meanpat(x);
 y=[ones(1,1000),(ones(1,1000)+1)];
 
 [W, ~] = pca(x);
@@ -22,3 +21,7 @@ x1=W*x;
 figure, plot(x(1,:), x(2,:),'*r'); hold on;
 plot(x1(1,:), x1(2,:),'.b'); 
 hold off; legend('Datos iniciales', 'Datos transformados LDA');
+
+%%
+% quedandome solo con los datos del eje x, claramente podemos ver que
+% existe mas diferenciacion usando fisher que usando pca
